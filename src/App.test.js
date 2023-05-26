de852f6a74232ea0df24e5bb7011b4f0580a2704
GabrielVanzo <gabriel.vanzo98@gmail.com>
2023-05-26 16:32:27 -0300

    fix bugs

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Aumente Suas Vendas Em Até 50% Mentoria Personalizada de 30 minutos/i);
   expect(linkElement).toBeInTheDocument();
 });
